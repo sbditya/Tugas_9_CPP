5fc5a40fb7ac0aca52577000bae43b3078cad274
Salsabila Ditya Anisa <sbditya320@gmail.com>
2021-09-01 13:16:27 +0700

    Tugas 9 CPP Niomic

---
+++ tugas 9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+	int x,z; 
+	
+	cout<<"Inputkan Nilai X : ";
+	cin>>x;
+	z = 0;
+	cout<<z<<endl;
+	
+	for(z = 1; z<=5;z++){
+		cout<<z*5<<endl;
+		
+	}
+}
